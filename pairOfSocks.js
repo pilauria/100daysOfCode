eb3824fe03a5d84c825f8f15b8d30115b986c7c0
Pietro Lauria <ptr.lauria@gmail.com>
2022-01-16 23:52:59 +0100

    10th

---
+++ pairOfSocks.js
@@ -0,0 +1,21 @@
+function sockMerchant(n, ar) {
+  // Write your code here
+  let socks = {};
+  let pairs = 0;
+
+  for (let item of ar) {
+    console.log('item', socks[item]);
+    console.log('socks:', socks);
+    socks[item] = socks[item] + 1 || 1;
+    console.log('socks after iteration', socks);
+
+    if (socks[item] % 2 === 0) {
+      pairs++;
+    }
+  }
+  return pairs;
+}
+
+console.log(sockMerchant(9, [10, 20, 20, 10, 10, 30, 50, 10, 20]));
+
+// function sockMercant(params) {}
