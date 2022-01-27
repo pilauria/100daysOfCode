aeb3335070fa7c63c2e9f8cc9cbf03a5231cf39f
Pietro Lauria <ptr.lauria@gmail.com>
2022-01-27 20:35:06 +0100

    14

--- HrepeatedString.js
+++ HrepeatedString.js
@@ -1,9 +1,29 @@
 function repeatedString(s, n) {
   // Write your code here
 
-  let string = s.repeat(n);
-
-  return string.split('a').length - 1;
+  let occurrences = s.split('a').length - 1;
+  console.log(occurrences);
+  let max = Math.floor(n / s.length);
+  console.log(max);
+  let totalA = occurrences * max;
+  totalA += s.slice(0, n % s.length).split('a').length - 1;
+  return totalA;
 }
+
+// (OR):
+const repeatedString = (s, n) => {
+  const strLen = s.length;
+  const fullStringRepetitions = Math.floor(n / strLen);
+  console.log('fullStringRepetitions:', fullStringRepetitions);
+  const trailingCharacterCount = n - strLen * fullStringRepetitions;
+  console.log('trailingCharacterCount:', trailingCharacterCount);
+  const trailingCharacters = s.slice(0, trailingCharacterCount);
+  console.log('trailingCharacters:', trailingCharacters);
+
+  const getCharCount = str => [...str].reduce((a, b) => a + (b === 'a'), 0);
+  return (
+    fullStringRepetitions * getCharCount(s) + getCharCount(trailingCharacters)
+  );
+};
 
 console.log(repeatedString('aba', 10));
