cb9f70d65ab9ebd106b90a9cd1cb6711f8a8a2e3
Pietro Lauria <ptr.lauria@gmail.com>
2022-02-16 22:40:55 +0100

    more

--- pureFunction.js
+++ pureFunction.js
@@ -8,3 +8,14 @@ accelerate(speed);
 accelerate(speed);
 accelerate(speed);
 console.log(accelerate(speed));
+
+var speed = 0;
+
+// const accelerate = speed => {
+//   return (speed += 1);
+// };
+
+// accelerate(speed);
+// accelerate(speed);
+// accelerate(speed);
+// console.log(accelerate(speed));
